cf1f14b01aa88d792b5974e0242a0c5eb665d67a
nazabe <nazarenobeauvais@gmail.com>
2025-04-01 17:39:12 -0300

    build(ota_example): update dependency

---
+++ examples/ota_example/main/main.c
@@ -0,0 +1,29 @@
+// Standard C libraries
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+
+// FreeRTOS
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+// ESP-IDF logging and system
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_sntp.h"
+#include "esp_heap_caps.h"
+#include "esp_crt_bundle.h"
+
+// MQTT
+#include "mqtt_client.h"
+
+#include "esp32_mqtt.h"
+
+void app_main(void)
+{
+
+}
